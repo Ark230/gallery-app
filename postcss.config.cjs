702c6e871aff844dad37e389c5d145a95790fcfa
Ignacio Pérez <iperez@jumpitt.com>
2024-02-01 17:42:40 -0300

    starting to build mobile first layout

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,8 @@
+const colors = require("./src/styles/settings/colors.css");
+
 module.exports = {
   plugins: [
+    require("postcss-simple-vars")({ variables: colors }),
     require("cssnano"),
     require("postcss-nested"),
     require("postcss-preset-env"),
